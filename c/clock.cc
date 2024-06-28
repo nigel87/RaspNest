16f2d69b880eed7e9e613a1576b224df73e767e9
Nigel Pllaha <nigelpllaha@gmail.com>
2024-06-28 12:56:45 +0200

    clock cc

--- c/clock.cc
+++ c/clock.cc
@@ -6,7 +6,7 @@
 // (but note, that the led-matrix library this depends on is GPL v2)
 
 #include "led-matrix.h"
-#include "graphics.h"
+#include "graphics.h" 
 
 #include <getopt.h>
 #include <signal.h>
