03e615e275d386843fbecbbdd145144c080de2c6
Nigel Pllaha <nigelpllaha@gmail.com>
2024-06-29 13:54:26 +0200

    scroll dynamic time

--- modes/news.py
+++ modes/news.py
@@ -1,18 +1,23 @@
- 
 import os
 import time
 import threading
 from python_server.scrolling_text_controller import start_scrolling_text, stop_scrolling_text
 import feedparser
 
+BASE_DISPLAY_TIME = 2
+SCALE_FACTOR = 0.135
 
+def calculate_display_time(text):
+    text_length = len(text)
+    display_time = int(BASE_DISPLAY_TIME + (text_length * SCALE_FACTOR))
+    return display_time
 
 def run(rss_feed_url, cpp_binary_folder, stop_event):
-    # Stop any existing scrolling text
     stop_scrolling_text()
 
     feed = feedparser.parse(rss_feed_url)
     cpp_binary = os.path.join(cpp_binary_folder, 'text-scroller')
+    
     if "title" in feed.feed:
         title = feed.feed.title
         # Display the title on the LED matrix
@@ -20,7 +25,7 @@ def run(rss_feed_url, cpp_binary_folder, stop_event):
                '--led-no-hardware-pulse', '--led-cols=64', '--led-gpio-mapping=adafruit-hat',
                '--led-slowdown-gpio=4']
         start_scrolling_text(args)
-        time.sleep(3)
+        time.sleep(calculate_display_time(title))
         stop_scrolling_text()
 
     for entry in feed.entries:
@@ -34,9 +39,11 @@ def run(rss_feed_url, cpp_binary_folder, stop_event):
                     '--led-slowdown-gpio=4']
             start_scrolling_text(args)
 
-            # Wait for a few seconds to display each entry
-            for _ in range(10):
-                if stop_event.is_set():
-                    break
+            display_time = calculate_display_time(entry_title)
+            start_time = time.time()
+
+            # Wait for calculated display time or until stop event is set
+            while not stop_event.is_set() and (time.time() - start_time) < display_time:
                 time.sleep(1)
+            
             stop_scrolling_text()
