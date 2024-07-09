dbf81ed064032012a943d69842563786c32c01eb
Nigel Pllaha <nigelpllaha@gmail.com>
2024-07-09 10:01:40 +0200

    added more stocks

--- python_server/modes/stock_market.py
+++ python_server/modes/stock_market.py
@@ -4,12 +4,15 @@ from python_server.shared.constants import GREEN, RED, GOLD
 from python_server.shared.service.stock_market_service import get_daily_price_change
 def run(stop_event):
     stop_scrolling_text()
+    symbols =[ "AAPL", "GOOG", "MSFT","AMZN", "NVDA"]
 
-    symbol = "AAPL"
 
-    daily_change = get_daily_price_change(symbol)
 
-    if daily_change is not None:
-        displayTitle = f"Daily price change for {symbol}: {daily_change:.2f}%"
-        print(displayTitle)
-        display_on_matrix(displayTitle, GREEN,stop_event)
+    for symbol in symbols:
+        daily_change = get_daily_price_change(symbol)
+        if daily_change is not None:
+            displayTitle = f"Daily price change for {symbol}: {daily_change:.2f}%"
+            print(displayTitle)
+            display_on_matrix
+            colour = GREEN if daily_change>0 else RED 
+            display_on_matrix(displayTitle, colour,stop_event)
