c7b2487614f3dfa05e343ccd818ed2a4c8ff2db6
Nigel Pllaha <nigelpllaha@gmail.com>
2024-07-29 12:16:26 +0200

    added SP500

--- python_server/modes/stock_market.py
+++ python_server/modes/stock_market.py
@@ -1,17 +1,17 @@
 from python_server.shared.controller.matrix_controller import stop_scrolling_text, display_on_matrix
-import feedparser
 from python_server.shared.constants import GREEN, RED, GOLD
 from python_server.shared.service.stock_market_service import get_daily_price_change
 def run(stop_event):
     stop_scrolling_text()
-    symbols =[ "AAPL", "GOOG", "MSFT","AMZN", "NVDA"]
+    symbols = ["SPY","AAPL", "GOOG", "MSFT", "AMZN", "NVDA"]
+
 
 
 
     for symbol in symbols:
         daily_change = get_daily_price_change(symbol)
         if daily_change is not None:
-            displayTitle = f"Daily price change for {symbol}: {daily_change:.2f}%"
+            displayTitle = f"{symbol}: {daily_change:.2f}%"
             print(displayTitle)
             display_on_matrix
             colour = GREEN if daily_change>0 else RED 
